bc3bf4b90ebd81f753bf711afdd8a904d07983e9
andresdude77 <andresdude77@yahoo.com>
2012-05-09 21:58:13 +0000

    Made the robot tilt to fix the problem of it falling

--- UHRoboCup/src/kick.py
+++ UHRoboCup/src/kick.py
@@ -32,8 +32,8 @@ def step2():
     LeftArm  = [+90, +90, +00, +00, +00, +00]
     RightArm = [+90, -00, -00, -00, +00, +00]
 
-    LeftLeg  = [+00, -15, -10, +00, +00, +10]
-    RightLeg = [+00, -20, -10, +20, -40, +00]
+    LeftLeg  = [+00, -7, -10, +00, +00, +10]
+    RightLeg = [+00, -20, -10, +20, -30, +00]
     pTargetAngles = (Head + LeftArm + LeftLeg + RightLeg + RightArm)
     pTargetAngles = [ x * motion.TO_RAD for x in pTargetAngles]
     return pTargetAngles
@@ -44,8 +44,8 @@ def step3():
     LeftArm  = [+90, +90, +00, +00, +00, +00]
     RightArm = [+90, -00, -00, -00, +00, +00]
 
-    LeftLeg  = [+00, -15, -10, +00, +00, +10]
-    RightLeg = [+00, -20, -10, +90, -40, +00]
+    LeftLeg  = [+00, -5, -10, +00, +00, +10]
+    RightLeg = [+00, -20, -10, +70, -40, +00]
     pTargetAngles = (Head + LeftArm + LeftLeg + RightLeg + RightArm)
     pTargetAngles = [ x * motion.TO_RAD for x in pTargetAngles]
     return pTargetAngles
