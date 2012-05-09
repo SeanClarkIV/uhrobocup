c8fb73f2be66cfb6b49c32bd028053a63d588fc3
alexandrojv9@gmail.com <alexandrojv9@gmail.com@c7e3a293-b54f-770b-ae63-5bdf134c42df>
2012-05-09 22:11:12 +0000

    speeding things up

--- trunk/UHRoboCup/src/uhrobocup.py
+++ trunk/UHRoboCup/src/uhrobocup.py
@@ -16,15 +16,15 @@ def main():
     pMaxSpeedFraction2 = 1.0
     # Ask motion to do this with a blocking call
     proxy.angleInterpolationWithSpeed(pNames, kick.stand(), pMaxSpeedFraction)
-    time.sleep(4)
+    time.sleep(2)
     proxy.angleInterpolationWithSpeed(pNames, kick.step1(), .1)
-    time.sleep(4)
+    time.sleep(2)
     proxy.angleInterpolationWithSpeed(pNames, kick.step2(), .05)
-    time.sleep(4)
+    time.sleep(2)
     proxy.angleInterpolationWithSpeed(pNames, kick.step3(), pMaxSpeedFraction)
-    time.sleep(4)
+    time.sleep(2)
     proxy.angleInterpolationWithSpeed(pNames, kick.step4(), pMaxSpeedFraction2)
-    time.sleep(4)
+    time.sleep(2)
     proxy.angleInterpolationWithSpeed(pNames, kick.stand(), pMaxSpeedFraction)
 
 if __name__ == "__main__":
