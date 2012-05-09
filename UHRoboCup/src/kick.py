9dd906d63dec7eb3a520a01fcd200ce5fffcbc59
alexandrojv9 <alexandrojv9@gmail.com>
2012-05-09 21:50:55 +0000

    defining kick class

---
+++ UHRoboCup/src/kick.py
@@ -0,0 +1,63 @@
+import motion
+
+def stand():
+    Head     = [+00, +00]
+
+    LeftArm  = [+90, +00, +00, +00, +00, +00]
+    RightArm = [+90, -00, -00, -00, +00, +00]
+
+    LeftLeg  = [+00, +00, -00, +00, +00, +00]
+    RightLeg = [+00, -00, -00, +00, -00, +00]
+
+    pTargetAngles = (Head + LeftArm + LeftLeg + RightLeg + RightArm)
+    pTargetAngles = [ x * motion.TO_RAD for x in pTargetAngles]
+    return pTargetAngles
+
+def step1():
+    Head     = [+00, +00]
+
+    LeftArm  = [+90, +90, +00, +00, +00, +00]
+    RightArm = [+90, -00, -00, -00, +00, +00]
+
+    LeftLeg  = [+00, -15, -10, +00, +00, +10]
+    RightLeg = [+00, -15, -10, +10, -20, +10]
+
+    pTargetAngles = (Head + LeftArm + LeftLeg + RightLeg + RightArm)
+    pTargetAngles = [ x * motion.TO_RAD for x in pTargetAngles]
+    return pTargetAngles
+
+def step2():
+    Head     = [+00, +00]
+
+    LeftArm  = [+90, +90, +00, +00, +00, +00]
+    RightArm = [+90, -00, -00, -00, +00, +00]
+
+    LeftLeg  = [+00, -15, -10, +00, +00, +10]
+    RightLeg = [+00, -20, -10, +20, -40, +00]
+    pTargetAngles = (Head + LeftArm + LeftLeg + RightLeg + RightArm)
+    pTargetAngles = [ x * motion.TO_RAD for x in pTargetAngles]
+    return pTargetAngles
+
+def step3():
+    Head     = [+00, +00]
+
+    LeftArm  = [+90, +90, +00, +00, +00, +00]
+    RightArm = [+90, -00, -00, -00, +00, +00]
+
+    LeftLeg  = [+00, -15, -10, +00, +00, +10]
+    RightLeg = [+00, -20, -10, +90, -40, +00]
+    pTargetAngles = (Head + LeftArm + LeftLeg + RightLeg + RightArm)
+    pTargetAngles = [ x * motion.TO_RAD for x in pTargetAngles]
+    return pTargetAngles
+
+def step4():
+    Head     = [+00, +00]
+
+    LeftArm  = [+90, +90, +00, +00, +00, +00]
+    RightArm = [+90, -00, -00, -00, +00, +00]
+
+    LeftLeg  = [+00, -10, -10, +00, +00, +10]
+    RightLeg = [+00, -10, -10, +00, -10, +00]
+    pTargetAngles = (Head + LeftArm + LeftLeg + RightLeg + RightArm)
+    pTargetAngles = [ x * motion.TO_RAD for x in pTargetAngles]
+    return pTargetAngles
