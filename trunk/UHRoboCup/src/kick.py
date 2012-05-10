e2888eca7660d147ea4a47344b7211f6a1c0bffc
alexandrojv9@gmail.com <alexandrojv9@gmail.com@c7e3a293-b54f-770b-ae63-5bdf134c42df>
2012-05-10 21:30:09 +0000

    kick working with robot

--- trunk/UHRoboCup/src/kick.py
+++ trunk/UHRoboCup/src/kick.py
@@ -16,11 +16,11 @@ def stand():
 def step1():
     Head     = [+00, +00]
 
-    LeftArm  = [+90, +120, +00, +00, +00, +00]
+    LeftArm  = [+90, +80, +00, +00, +00, +00]
     RightArm = [+90, -00, -00, -00, +00, +00]
 
-    LeftLeg  = [+00, +7, -10, +00, +00, +10]
-    RightLeg = [+00, +7, -10, +20, +00, +10]
+    LeftLeg  = [+00, +5, -10, +00, +00, +10]
+    RightLeg = [+00, +5, -10, +20, +00, +10]
 
     pTargetAngles = (Head + LeftArm + LeftLeg + RightLeg + RightArm)
     pTargetAngles = [ x * motion.TO_RAD for x in pTargetAngles]
@@ -29,10 +29,10 @@ def step1():
 def step2():
     Head     = [+00, +00]
 
-    LeftArm  = [+90, +90, +00, +00, +00, +00]
+    LeftArm  = [+90, +80, +00, +00, +00, +00]
     RightArm = [+90, -00, -00, -00, +00, +00]
 
-    LeftLeg  = [+00, +10, -10, +00, +00, +10]
+    LeftLeg  = [+00, +5, -10, +00, +00, +10]
     RightLeg = [+00, -00, -10, +20, -30, +00]
     pTargetAngles = (Head + LeftArm + LeftLeg + RightLeg + RightArm)
     pTargetAngles = [ x * motion.TO_RAD for x in pTargetAngles]
@@ -41,10 +41,10 @@ def step2():
 def step3():
     Head     = [+00, +00]
 
-    LeftArm  = [+90, +90, +00, +00, +00, +00]
-    RightArm = [+90, -00, -00, -00, +00, +00]
+    LeftArm  = [+90, +80, +00, +00, +00, +00]
+    RightArm = [+180, -00, -00, -00, +00, +00]
 
-    LeftLeg  = [+00, +10, -15, +00, +00, +10]
+    LeftLeg  = [+00, +5, -15, +00, +00, +10]
     RightLeg = [+00, -00, -10, +70, -40, +00]
     pTargetAngles = (Head + LeftArm + LeftLeg + RightLeg + RightArm)
     pTargetAngles = [ x * motion.TO_RAD for x in pTargetAngles]
@@ -53,10 +53,10 @@ def step3():
 def step4():
     Head     = [+00, +00]
 
-    LeftArm  = [+90, +90, +00, +00, +00, +00]
-    RightArm = [+90, -00, -00, -00, +00, +00]
+    LeftArm  = [+90, +80, +00, +00, +00, +00]
+    RightArm = [+180, -00, -00, -00, +00, +00]
 
-    LeftLeg  = [+00, +10, -20, +00, +00, +10]
+    LeftLeg  = [+00, +5, -20, +00, +00, +10]
     RightLeg = [+00, -00, -60, +00, +50, +00]
     pTargetAngles = (Head + LeftArm + LeftLeg + RightLeg + RightArm)
     pTargetAngles = [ x * motion.TO_RAD for x in pTargetAngles]
