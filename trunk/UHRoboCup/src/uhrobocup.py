fbd8d2e9a91a18ab6ec1ef4e10ae57ab5a5ee905
andresdude77@yahoo.com <andresdude77@yahoo.com@c7e3a293-b54f-770b-ae63-5bdf134c42df>
2012-05-10 22:52:40 +0000

    Added a walk into the code, made it go from walk directly to step one. No stand necissary.anded .05 to speed of step one. played with walk a little bit.

--- trunk/UHRoboCup/src/uhrobocup.py
+++ trunk/UHRoboCup/src/uhrobocup.py
@@ -1,13 +1,14 @@
 import config
 import time
 import kick
-import walk
+import motion
 
 def main():
     proxy = config.loadProxy("ALMotion")
 
     # Turn Stiffness On in order to have the robot do moves.
     config.StiffnessOn(proxy)
+    config.PoseInit(proxy, .5)
 
     #------------------------------ send the commands -----------------------------
     # We use the "Body" name to signify the collection of all joints
@@ -15,12 +16,32 @@ def main():
     # We set the fraction of max speed
     pMaxSpeedFraction = 0.1
     # Ask motion to do this with a blocking call
+    # walking section
+    motionProxy = config.loadProxy("ALMotion")
+    motionProxy.setMotionConfig([["ENABLE_FOOT_CONTACT_PROTECTION", True]])
+
+    #TARGET VELOCITY
+    X = 1.0
+    Y = 0.0
+    Theta = 0.0
+    Frequency =1.0 # max speed
+    motionProxy.setWalkTargetVelocity(X, Y, Theta, Frequency)
+    time.sleep(4.0)
+
+    #####################
+    ## End Walk
+    #####################
+    #TARGET VELOCITY
+    X = 0.0
+    Y = 0.0
+    Theta = 0.0
+    motionProxy.setWalkTargetVelocity(X, Y, Theta, Frequency)
+
 
     #Kicking section
 
-    proxy.angleInterpolationWithSpeed(pNames, kick.stand(), .6)
     
-    proxy.angleInterpolationWithSpeed(pNames, kick.step1(), 0.1)
+    proxy.angleInterpolationWithSpeed(pNames, kick.step1(), 0.15)
     
     proxy.angleInterpolationWithSpeed(pNames, kick.step2(), 1.0)
     
