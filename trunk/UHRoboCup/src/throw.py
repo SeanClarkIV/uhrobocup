85b0afc13944ad9708dcbddf402c38504d828cca
andresdude77@yahoo.com <andresdude77@yahoo.com@c7e3a293-b54f-770b-ae63-5bdf134c42df>
2012-05-21 22:05:37 +0000

    trying to start a throwing for the goalie...

---
+++ trunk/UHRoboCup/src/throw.py
@@ -0,0 +1,101 @@
+import config
+import motion
+from naoqi import ALProxy
+
+proxy = config.loadProxy("ALMotion")
+pNames = "Body"
+tts = ALProxy("ALTextToSpeech", config.IP, 9559)
+
+def stand():
+    Head     = [ + 00, + 90]
+
+    LeftArm  = [ + 90, + 00, + 00, + 00, + 00, + 00]
+    RightArm = [ + 90, -00, -00, -00, + 00, + 00]
+
+    LeftLeg  = [ + 00, + 00, -00, + 00, + 00, + 00]
+    RightLeg = [ + 00, -00, -00, + 00, -00, + 00]
+
+    pTargetAngles = (Head + LeftArm + LeftLeg + RightLeg + RightArm)
+    pTargetAngles = [x * motion.TO_RAD for x in pTargetAngles]
+    return pTargetAngles
+
+def step1():
+    Head     = [ + 00, + 00]
+
+    LeftArm  = [ +90, +00, - 90, - 00, -180, + 40]
+    RightArm = [ +120, +00, -00, +00, +90, + 00]
+
+    LeftLeg  = [ + 00, + 00, -90, + 120, - 40, - 00]
+    RightLeg = [ + 00, + 00, -90, + 120, - 40, - 00]
+
+    pTargetAngles = (Head + LeftArm + LeftLeg + RightLeg + RightArm)
+    pTargetAngles = [x * motion.TO_RAD for x in pTargetAngles]
+    return pTargetAngles
+
+def step2():
+    Head     = [ + 00, + 00]
+
+    LeftArm  = [ +90, +00, - 90, - 00, -180, + 10]
+    RightArm = [ +120, +00, -00, +00, +90, + 00]
+
+    LeftLeg  = [ + 00, + 00, -90, + 120, - 40, - 00]
+    RightLeg = [ + 00, + 00, -90, + 120, - 40, - 00]
+
+    pTargetAngles = (Head + LeftArm + LeftLeg + RightLeg + RightArm)
+    pTargetAngles = [x * motion.TO_RAD for x in pTargetAngles]
+    return pTargetAngles
+def step3():
+    Head     = [ + 00, + 00]
+
+    LeftArm  = [ +00, +00, - 90, - 00, -180, + 35]
+    RightArm = [ +120, +00, -00, +00, +90, + 00]
+
+    LeftLeg  = [ + 00, + 00, -90, + 120, - 40, - 00]
+    RightLeg = [ + 00, + 00, -90, + 120, - 40, - 00]
+
+    pTargetAngles = (Head + LeftArm + LeftLeg + RightLeg + RightArm)
+    pTargetAngles = [x * motion.TO_RAD for x in pTargetAngles]
+    return pTargetAngles
+def step4():
+    Head     = [ + 00, + 00]
+
+    LeftArm  = [ +90, +00, - 90, - 00, -180, + 10]
+    RightArm = [ +120, +00, -00, +00, +90, + 00]
+
+    LeftLeg  = [ + 00, + 00, -90, + 120, - 40, - 00]
+    RightLeg = [ + 00, + 00, -90, + 120, - 40, - 00]
+
+    pTargetAngles = (Head + LeftArm + LeftLeg + RightLeg + RightArm)
+    pTargetAngles = [x * motion.TO_RAD for x in pTargetAngles]
+    return pTargetAngles
+def step5():
+    Head     = [ + 00, + 00]
+
+    LeftArm  = [ +90, +90, - 90, - 00, -180, + 10]
+    RightArm = [ +120, +00, -00, +00, +90, + 00]
+
+    LeftLeg  = [ + 00, + 00, -90, + 120, - 40, - 00]
+    RightLeg = [ + 00, + 00, -90, + 120, - 40, - 00]
+
+    pTargetAngles = (Head + LeftArm + LeftLeg + RightLeg + RightArm)
+    pTargetAngles = [x * motion.TO_RAD for x in pTargetAngles]
+    return pTargetAngles
+#This function kicks the ball, y being an input of what to say right before he
+#kicks the ball.
+
+#rightkick kicks with right foot
+def throwstrait(textToSpeech):
+    #Turns the stiffness on.
+    config.StiffnessOn(proxy)
+    proxy.angleInterpolationWithSpeed(pNames, step1(), 0.5)
+    proxy.angleInterpolationWithSpeed(pNames, step2(), 0.01)
+    proxy.angleInterpolationWithSpeed(pNames, step3(), 1.0)
+
+
+#leftkick kicks with left foot
+def throwright(textToSpeech):
+    #Turns the stiffness on.
+    config.StiffnessOn(proxy)
+    proxy.angleInterpolationWithSpeed(pNames, step1(), 0.5)
+    proxy.angleInterpolationWithSpeed(pNames, step4(), 0.01)
+    proxy.angleInterpolationWithSpeed(pNames, step5(), 1.0)
