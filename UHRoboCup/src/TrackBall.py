3d924c01ad3963bef82dc4d16aaf776aaf44b995
alexandrojv9 <alexandrojv9@gmail.com>
2012-05-22 04:41:47 +0000

    Tracking red ball initial test

---
+++ UHRoboCup/src/TrackBall.py
@@ -0,0 +1,22 @@
+import time
+import config
+from naoqi import ALProxy
+
+motion = ALProxy("ALMotion", config.IP, 9559)
+redBallTracker = ALProxy("ALRedBallTracker", config.IP, 9559)
+
+# Set Head Stiffness is ON and start red ball tracker
+motion.setStiffnesses("Head", 1.0)
+print "Head stiffness ON."
+redBallTracker.startTracker()
+print "Looking for red ball to track."
+
+# Track red ball for 60 seconds.
+time.sleep(60)
+print "Ball tracking will be stopped in 60 seconds...."
+
+# Stop tracker and remove head stiffness.
+redBallTracker.stopTracker()
+print "Red ball traking stoped."
+motion.setStiffnesses("Head", 0.0)
+print "Head stiffness OFF."
