f2d9e87ffdf3f8d7736476b652962b33f9c6138c
alexandrojv9@gmail.com <alexandrojv9@gmail.com@c7e3a293-b54f-770b-ae63-5bdf134c42df>
2012-05-29 15:59:07 +0000

    Menu in main

--- trunk/UHRoboCup/src/main.py
+++ trunk/UHRoboCup/src/main.py
@@ -2,36 +2,43 @@ import config
 import kick
 from naoqi import ALProxy
 import walk
-import throw
+
+# Common Variables
+proxy = config.loadProxy("ALMotion")
+tts = ALProxy("ALTextToSpeech", config.IP, 9559)
 
 def main():
+    def menu():
+        #print what options you have
+        print "\n*** OPTIONS ***"
+        print "0) Quit"
+        print "1) Turn stiffness ON"
+        print "2) Turn stiffness OFF"
+        print "3) Initial Pose"
+        print "4) Left Kick"
+        print "5) Right Kick"
+        print "6) Walk"
+        return input ("\nChoose your option: ")
 
-    # Common Variables
-    proxy = config.loadProxy("ALMotion")
-    tts = ALProxy("ALTextToSpeech", config.IP, 9559)
+    # NOW THE PROGRAM REALLY STARTS, AS CODE IS RUN
+    loop = 1
+    choice = 0
+    while loop == 1:
+        choice = menu()
+        if choice == 0:
+            loop = 0
+        elif choice == 1:
+            config.StiffnessOn(proxy)
+        elif choice == 2:
+            config.StiffnessOff(proxy)
+        elif choice == 3:
+            config.PoseInit(proxy, 0.5)
+        elif choice == 4:
+            kick.kickLeftFoot()
+        elif choice == 5:
+            kick.kickRightFoot()
+        elif choice == 6:
+            walk.walkTimed(5)
 
-    # Turn Stiffness On in order to have the robot do moves and not fall.
-    config.StiffnessOn(proxy)
-    # Switch pose to initial at half the max speed.
-    config.PoseInit(proxy, .5)
-
-    # Walk for specified time in seconds.
-
-    # Kicking Left Foot while saying specified text.
-    kick.kickLeftFoot()
-    kick.kickRightFoot()
-    # Walk for specified time in seconds.
-    config.PoseInit(proxy, .2)
-
-    # Say specified text.
-    tts.say("Team. maqena, erektous. University of Houston. Go cougars")
-
-def main2():
-    proxy = config.loadProxy("ALMotion")
-
-    # Turn Stiffness On in order to have the robot do moves and not fall.
-    config.StiffnessOn(proxy)
-    # throws ball
-    throw.throwstrait()
 if __name__ == "__main__":
     main()
