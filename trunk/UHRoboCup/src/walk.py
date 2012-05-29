e9346343834256d9c96e40a2fa2bb7ce726b8b79
andresdude77@yahoo.com <andresdude77@yahoo.com@c7e3a293-b54f-770b-ae63-5bdf134c42df>
2012-05-29 22:45:11 +0000

    Added new inputs and now you can make him speak also, turn right left and go backwards.

--- trunk/UHRoboCup/src/walk.py
+++ trunk/UHRoboCup/src/walk.py
@@ -1,6 +1,5 @@
 import time
 import config
-import motion
 
 """ WALKING STEPS GO HERE
 
@@ -20,7 +19,7 @@ def walkStep1():
     return pTargetAngles
 """
 
-def walkTimed(walkTime):
+def walkFowardTimed(walkTime):
 
     #This walk walks for a walkTime amount of time and then stops. 
     motionProxy = config.loadProxy("ALMotion")
@@ -32,6 +31,58 @@ def walkTimed(walkTime):
 
     motionProxy.setWalkTargetVelocity(X, Y, Theta, Frequency)
     time.sleep(walkTime)
+
+    #Set the velocity to zero to stop the walk
+    X = 0.0
+    Y = 0.0
+    Theta = 0.0
+    motionProxy.setWalkTargetVelocity(X, Y, Theta, Frequency)
+def walkBackTimed(walkTime):
+
+    #This walk walks for a walkTime amount of time and then stops.
+    motionProxy = config.loadProxy("ALMotion")
+    motionProxy.setMotionConfig([["ENABLE_FOOT_CONTACT_PROTECTION", True]])
+    X = -0.8
+    Y = 0.0
+    Theta = 0.0
+    Frequency = 1.0 # max speed
+
+    motionProxy.setWalkTargetVelocity(X, Y, Theta, Frequency)
+    time.sleep(walkTime)
+
+    #Set the velocity to zero to stop the walk
+    X = 0.0
+    Y = 0.0
+    Theta = 0.0
+    motionProxy.setWalkTargetVelocity(X, Y, Theta, Frequency)
+def turnleft():
+
+    #This walk walks for a walkTime amount of time and then stops.
+    motionProxy = config.loadProxy("ALMotion")
+    motionProxy.setMotionConfig([["ENABLE_FOOT_CONTACT_PROTECTION", True]])
+    X = 0.0
+    Y = 0.0
+    Theta = 1.0
+    Frequency = 1.0 # max speed
+    motionProxy.setWalkTargetVelocity(X, Y, Theta, Frequency)
+    time.sleep(2)
+
+    #Set the velocity to zero to stop the walk
+    X = 0.0
+    Y = 0.0
+    Theta = 0.0
+    motionProxy.setWalkTargetVelocity(X, Y, Theta, Frequency)
+def turnright():
+
+    #This walk walks for a walkTime amount of time and then stops.
+    motionProxy = config.loadProxy("ALMotion")
+    motionProxy.setMotionConfig([["ENABLE_FOOT_CONTACT_PROTECTION", True]])
+    X = 0.0
+    Y = 0.0
+    Theta = -1.0
+    Frequency = 1.0 # max speed
+    motionProxy.setWalkTargetVelocity(X, Y, Theta, Frequency)
+    time.sleep(2)
 
     #Set the velocity to zero to stop the walk
     X = 0.0
