61ac1336628e88ae1966931078da17d694a3b912
andresdude77 <andresdude77@yahoo.com>
2012-05-29 21:33:51 +0000

    Made the input a list and this makes it able to take multiple commands to do in a row.

--- UHRoboCup/src/main.py
+++ UHRoboCup/src/main.py
@@ -18,27 +18,46 @@ def main():
         print "4) Left Kick"
         print "5) Right Kick"
         print "6) Walk"
-        return input ("\nChoose your option: ")
+        return input ("\nChoose your option(s) as list: ")
 
     # NOW THE PROGRAM REALLY STARTS, AS CODE IS RUN
     loop = 1
     choice = 0
     while loop == 1:
         choice = menu()
-        if choice == 0:
-            loop = 0
-        elif choice == 1:
-            config.StiffnessOn(proxy)
-        elif choice == 2:
-            config.StiffnessOff(proxy)
-        elif choice == 3:
-            config.PoseInit(proxy, 0.5)
-        elif choice == 4:
-            kick.kickLeftFoot()
-        elif choice == 5:
-            kick.kickRightFoot()
-        elif choice == 6:
-            walk.walkTimed(5)
-
+        if len(choice) == 1:
+            if choice[0] == 0:
+                loop = 0
+            elif choice[0] == 1:
+                config.StiffnessOn(proxy)
+            elif choice[0] == 2:
+                config.StiffnessOff(proxy)
+            elif choice[0] == 3:
+                config.PoseInit(proxy, 0.5)
+            elif choice[0] == 4:
+                kick.kickLeftFoot()
+            elif choice[0] == 5:
+                kick.kickRightFoot()
+            elif choice[0] == 6:
+                walk.walkTimed(5)
+        if len(choice) >1:
+            k=0
+            while k < len(choice):
+                if choice[k] == 0:
+                    loop = 0
+                    break
+                elif choice[k] == 1:
+                    config.StiffnessOn(proxy)
+                elif choice[k] == 2:
+                    config.StiffnessOff(proxy)
+                elif choice[k] == 3:
+                    config.PoseInit(proxy, 0.5)
+                elif choice[k] == 4:
+                    kick.kickLeftFoot()
+                elif choice[k] == 5:
+                    kick.kickRightFoot()
+                elif choice[k] == 6:
+                    walk.walkTimed(5)
+                k = k+1
 if __name__ == "__main__":
     main()
