f0e17f736b67975acd4a0884b265086e2f2d1cfb
andresdude77@yahoo.com <andresdude77@yahoo.com@c7e3a293-b54f-770b-ae63-5bdf134c42df>
2012-05-31 17:37:16 +0000

    Optomized stand on back, made a stand on front code

--- trunk/UHRoboCup/src/stand.py
+++ trunk/UHRoboCup/src/stand.py
@@ -3,117 +3,8 @@ import motion
 
 proxy = config.loadProxy("ALMotion")
 pNames = "Body"
-def crouch():
-  ''' Makes NAO crouch. '''
-  names = list()
-  times = list()
-  keys = list()
-
-  names.append("HeadYaw")
-  times.append([ 1.00000])
-  keys.append([ -0.03379])
-
-  names.append("HeadPitch")
-  times.append([ 1.00000])
-  keys.append([ 0.32823])
-
-  names.append("LShoulderPitch")
-  times.append([ 1.00000])
-  keys.append([ 1.62600])
-
-  names.append("LShoulderRoll")
-  times.append([ 1.00000])
-  keys.append([ -0.01998])
-
-  names.append("LElbowYaw")
-  times.append([ 1.00000])
-  keys.append([ -0.98640])
-
-  names.append("LElbowRoll")
-  times.append([ 1.00000])
-  keys.append([ -1.24250])
-
-  names.append("LWristYaw")
-  times.append([ 1.00000])
-  keys.append([ 0.08433])
-
-  names.append("LHand")
-  times.append([ 1.00000])
-  keys.append([ 0.00015])
-
-  names.append("RShoulderPitch")
-  times.append([ 1.00000])
-  keys.append([ 1.51870])
-
-  names.append("RShoulderRoll")
-  times.append([ 1.00000])
-  keys.append([ -0.05680])
-
-  names.append("RElbowYaw")
-  times.append([ 1.00000])
-  keys.append([ 0.72554])
-
-  names.append("RElbowRoll")
-  times.append([ 1.00000])
-  keys.append([ 1.26099])
-
-  names.append("RWristYaw")
-  times.append([ 1.00000])
-  keys.append([ 0.72094])
-
-  names.append("RHand")
-  times.append([ 1.00000])
-  keys.append([ 0.00018])
-
-  names.append("LHipYawPitch")
-  times.append([ 1.00000])
-  keys.append([ 0.01538])
-
-  names.append("LHipRoll")
-  times.append([ 1.00000])
-  keys.append([ -0.00456])
-
-  names.append("LHipPitch")
-  times.append([ 1.00000])
-  keys.append([ -0.90962])
-
-  names.append("LKneePitch")
-  times.append([ 1.00000])
-  keys.append([ 2.11255])
-
-  names.append("LAnklePitch")
-  times.append([ 1.00000])
-  keys.append([ -1.18889])
-
-  names.append("LAnkleRoll")
-  times.append([ 1.00000])
-  keys.append([ 0.00464])
-
-  names.append("RHipRoll")
-  times.append([ 1.00000])
-  keys.append([ 0.10589])
-
-  names.append("RHipPitch")
-  times.append([ 1.00000])
-  keys.append([ -0.90203])
-
-  names.append("RKneePitch")
-  times.append([ 1.00000])
-  keys.append([ 2.11255])
-
-  names.append("RAnklePitch")
-  times.append([ 1.00000])
-  keys.append([ -1.18630])
-
-  names.append("RAnkleRoll")
-  times.append([ 1.00000])
-  keys.append([ -0.10427])
-
-  proxy.angleInterpolation(names, keys, times, True)
-
-
 def stand():
-    Head     = [+0, +90]
+    Head     = [+0, +0]
 
     LeftArm  = [+90, +0, +0, +0, +0, +0]
     RightArm = [+90, +0, +0, +0, +0, +0]
@@ -223,16 +114,101 @@ def step7():
     pTargetAngles = (Head + LeftArm + LeftLeg + RightLeg + RightArm)
     pTargetAngles = [x * motion.TO_RAD for x in pTargetAngles]
     return pTargetAngles
+def step8():
+    Head     = [+0, -90]
 
-def standup():# Kick ball with right foot.
+    LeftArm  = [-90, -90, -90, -90, +0, +0]
+    RightArm = [-90, +90, +90, +90, +0, +0]
+
+    LeftLeg  = [-90, +90, -40, +00, +70, -00]
+    RightLeg = [-90, -90, -40, +00, +70, +00]
+
+    pTargetAngles = (Head + LeftArm + LeftLeg + RightLeg + RightArm)
+    pTargetAngles = [x * motion.TO_RAD for x in pTargetAngles]
+    return pTargetAngles
+def step9():
+    Head     = [+0, -90]
+
+    LeftArm  = [+40, -20, -00, -00, +00, +0]
+    RightArm = [+40, +20, +00, +00, +00, +0]
+
+    LeftLeg  = [-90, +90, -180, +00, +70, -00]
+    RightLeg = [-90, -90, -180, +00, +70, +00]
+
+    pTargetAngles = (Head + LeftArm + LeftLeg + RightLeg + RightArm)
+    pTargetAngles = [x * motion.TO_RAD for x in pTargetAngles]
+    return pTargetAngles
+def step10():
+    Head     = [+0, -90]
+
+    LeftArm  = [+00, +90, -00, -0, +0, +0]
+    RightArm = [+00, -90, +00, +0, +0, +0]
+
+    LeftLeg  = [-90, +90, -100, +0, +90, -00]
+    RightLeg = [-90, -90, -100, +0, +90, +00]
+
+    pTargetAngles = (Head + LeftArm + LeftLeg + RightLeg + RightArm)
+    pTargetAngles = [x * motion.TO_RAD for x in pTargetAngles]
+    return pTargetAngles
+def step11():
+    Head     = [+0, -90]
+
+    LeftArm  = [+40, +90, +0, -00, +0, +0]
+    RightArm = [+40, -90, -0, +00, +0, +0]
+
+    LeftLeg  = [-90, +90, -100, +20, +90, -00]
+    RightLeg = [-90, -90, -100, +20, +90, +00]
+
+    pTargetAngles = (Head + LeftArm + LeftLeg + RightLeg + RightArm)
+    pTargetAngles = [x * motion.TO_RAD for x in pTargetAngles]
+    return pTargetAngles
+def step12():
+    Head     = [+0, -90]
+
+    LeftArm  = [-45, +0, +0, -00, +0, +0]
+    RightArm = [-45, -0, -0, +00, +0, +0]
+
+    LeftLeg  = [-0, +0, -100, +0, +0, -00]
+    RightLeg = [-0, -0, -100, +0, +0, +00]
+
+    pTargetAngles = (Head + LeftArm + LeftLeg + RightLeg + RightArm)
+    pTargetAngles = [x * motion.TO_RAD for x in pTargetAngles]
+    return pTargetAngles
+def step13():
+    Head     = [+0, -90]
+
+    LeftArm  = [-90, +00, +0, -00, +0, +0]
+    RightArm = [-90, +00, -0, +00, +0, +0]
+
+    LeftLeg  = [-0, +0, -00, +0, -0, -00]
+    RightLeg = [-0, -0, -00, +0, -0, +00]
+
+    pTargetAngles = (Head + LeftArm + LeftLeg + RightLeg + RightArm)
+    pTargetAngles = [x * motion.TO_RAD for x in pTargetAngles]
+    return pTargetAngles
+def standuponback():# Kick ball with right foot.
     #Turns the stiffness on.
     config.StiffnessOn(proxy)
-    proxy.angleInterpolationWithSpeed(pNames, step(), 0.5)
-    proxy.angleInterpolationWithSpeed(pNames, step1(), 0.5)
+    proxy.angleInterpolationWithSpeed(pNames, step(), 0.8)
+    proxy.angleInterpolationWithSpeed(pNames, step1(), 0.8)
     proxy.angleInterpolationWithSpeed(pNames, step2(), 1.0)
-    proxy.angleInterpolationWithSpeed(pNames, step3(), 0.5)
-    proxy.angleInterpolationWithSpeed(pNames, step4(), 0.2)
-    proxy.angleInterpolationWithSpeed(pNames, step5(), 0.3)
-    proxy.angleInterpolationWithSpeed(pNames, step6(), 0.2)
-    proxy.angleInterpolationWithSpeed(pNames, step7(), 0.2)
-    proxy.angleInterpolationWithSpeed(pNames, stand(), 0.2)
+    proxy.angleInterpolationWithSpeed(pNames, step3(), 0.6)
+    proxy.angleInterpolationWithSpeed(pNames, step4(), 0.5)
+    proxy.angleInterpolationWithSpeed(pNames, step5(), 0.46)
+    proxy.angleInterpolationWithSpeed(pNames, step6(), 0.45)
+    proxy.angleInterpolationWithSpeed(pNames, step7(), 0.4)
+    proxy.angleInterpolationWithSpeed(pNames, stand(), 0.4)
+def standuponfront():# Kick ball with right foot.
+    #Turns the stiffness on.
+    config.StiffnessOn(proxy)
+    proxy.angleInterpolationWithSpeed(pNames, step13(), 0.1)
+    proxy.angleInterpolationWithSpeed(pNames, step8(), 0.1)
+    proxy.angleInterpolationWithSpeed(pNames, step9(), 0.2)
+    proxy.angleInterpolationWithSpeed(pNames, step10(), 0.2)
+    proxy.angleInterpolationWithSpeed(pNames, step11(), 0.1)
+    proxy.angleInterpolationWithSpeed(pNames, step12(), 0.1)
+    proxy.angleInterpolationWithSpeed(pNames, step4(), 0.1)
+    proxy.angleInterpolationWithSpeed(pNames, step5(), 0.1)
+    proxy.angleInterpolationWithSpeed(pNames, step6(), 0.1)
+    proxy.angleInterpolationWithSpeed(pNames, step7(), 0.1)
+    proxy.angleInterpolationWithSpeed(pNames, stand(), 0.1)
