af1995c09bc0bf1b4293d0ec8035518988c5ac33
bryan_lopez9655@yahoo.com <bryan_lopez9655@yahoo.com@c7e3a293-b54f-770b-ae63-5bdf134c42df>
2012-06-07 18:38:27 +0000

    Ultimate walk which is faster and more steady

--- trunk/UHRoboCup/src/walk.py
+++ trunk/UHRoboCup/src/walk.py
@@ -89,3 +89,27 @@ def turnright():
     Y = 0.0
     Theta = 0.0
     motionProxy.setWalkTargetVelocity(X, Y, Theta, Frequency)
+
+def ultimtewalkto():
+    # Set NAO in stiffness On
+    configs.StiffnessOn(motionProxy)
+
+    x     = 1
+    y     = 0.0
+    theta = 0.0
+
+    # This example show customization for the both foot
+    # with all the possible gait parameters
+    motionProxy.walkTo(x, y, theta,
+        [ ["MaxStepX", 0.08],         # step of x cm in front
+          ["MaxStepY", 0.101],         # default value
+          ["MaxStepTheta", 0.4],      # default value
+          ["MaxStepFrequency", 1.667],  # low frequency
+          ["StepHeight", 0.007],       # step height of x cm
+          ["TorsoWx", 0],           # default value
+          ["TorsoWy", -0.1] ])         # torso bend 0.1 rad in front
+
+    # This example show customization for the both foot
+    # with just one gait parameter, in this case, the other
+    # parameters are set to the default value
+    motionProxy.walkTo(x, y, theta, [ ["StepHeight", 0.007] ]) # step height of 4 cm
