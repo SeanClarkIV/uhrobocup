756b4cde7101ca04693ed6ed9e7ed4a47ef5b9b7
alexandrojv9 <alexandrojv9@gmail.com>
2012-06-21 17:00:59 +0000

    Commenting out on camera

--- UHRoboCup/src/camera.py
+++ UHRoboCup/src/camera.py
@@ -3,10 +3,10 @@ from naoqi import ALProxy
 
 videodevice = ALProxy("ALVideoDevice", config.IP, 9559)
 
-def topCamera():
+def topCamera(): # Activate top camera.
     kCameraSelectID = 18
     videodevice.setParam(kCameraSelectID, 0)
 
-def bottomCamera():
+def bottomCamera(): # Activate bottom camera.
     kCameraSelectID = 18
     videodevice.setParam(kCameraSelectID, 1)
