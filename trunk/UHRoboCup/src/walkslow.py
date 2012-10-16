6eb06e90d81e3478d38e44cc4e74941e7835f878
bryan_lopez9655@yahoo.com <bryan_lopez9655@yahoo.com@c7e3a293-b54f-770b-ae63-5bdf134c42df>
2012-10-16 00:01:02 +0000

    Fast Walk slowed down

---
+++ trunk/UHRoboCup/src/walkslow.py
@@ -0,0 +1,41 @@
+import config
+
+motionProxy = config.loadProxy("ALMotion")
+
+def ewalk():
+
+    #This walk walks for a walkTime amount of time and then stops.
+    useSensors = True
+    motionProxy.setMotionConfig([["ENABLE_FOOT_CONTACT_PROTECTION", True]])
+    angles = motionProxy.getAngles("Head", useSensors)
+    X = 2.0
+    Y = 0.0
+    Theta = angles[0]/2.1
+    Frequency = 1
+
+    motionProxy.setWalkTargetVelocity(X, Y, Theta, Frequency)
+def stop():
+
+    #This walk walks for a walkTime amount of time and then stops.
+    motionProxy.setMotionConfig([["ENABLE_FOOT_CONTACT_PROTECTION", True]])
+    X = 0
+    Y = 0.0
+    Theta = 0
+    Frequency = 0
+
+    motionProxy.setWalkTargetVelocity(X, Y, Theta, Frequency)
+
+def ultimatewalkto(x, y, z):
+    # Set NAO in stiffness On
+    config.StiffnessOn()
+
+    # This example show customization for the both foot
+    # with all the possible gait parameters
+    motionProxy.walkTo(x, y, z,
+        [ ["MaxStepX", 0.08],         # step of x cm in front
+          ["MaxStepY", 0.2],         # default value
+          ["MaxStepTheta", 0.4],      # default value
+          ["MaxStepFrequency", 1.667],  # low frequency
+          ["StepHeight", 0.015],       # step height of x cm
+          ["TorsoWx", 0],           # default value
+          ["TorsoWy", -0.1] ])         # torso bend 0.1 rad in front
