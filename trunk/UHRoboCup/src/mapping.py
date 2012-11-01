2d286fc75c92af65568bb0befb0bc2844f98858b
alexandrojv9@gmail.com <alexandrojv9@gmail.com@c7e3a293-b54f-770b-ae63-5bdf134c42df>
2012-11-01 22:27:19 +0000

    Updated naoMapping to show actual NAO position.

--- trunk/UHRoboCup/src/mapping.py
+++ trunk/UHRoboCup/src/mapping.py
@@ -11,21 +11,29 @@ def map():
     maplocation(localization.getnaolocation(),localization.getlocation())
 
 
-def maplocation((naoX, naoY, naoID), (ballX, ballY)):
+def maplocation((naoX, naoY, naoZ, naoID), (ballX, ballY)):
     currentFolder = (os.getcwd() + "\\")
     field = Image.open(currentFolder + "robocupfield.jpg")  # Open field image
     draw = ImageDraw.Draw(field)    # Select field image to draw to
 
+    offset = 76.45489365
+
+    naoX = ((naoX * offset) + 70)
+    naoY = ((naoY * -offset) + 70)
+
     draw.point([naoX, naoY])   # Draw point based on location received
-    print (naoID, naoX, naoY)
+    print (naoX, naoY, naoZ, naoID)
+
+    print (naoX, naoY, naoZ, naoID)
     radius = 8  # Radius of NAO based on foot length
     box = ((naoX - radius), (naoY - radius), (naoX + radius), (naoY + radius)) # Formulates the size of the box to crop in pixels (left, upper, right, bottom)
     draw.ellipse(box)    # Daw circle around point received
 
-    ballX = naoX + (ballX*76.45489365)
-    ballY = naoY + (ballY*-76.45489365)
+    ballX = naoX + (ballX * offset)
+    ballY = naoY + (ballY * -offset)
     draw.point([ballX, ballY])   # Draw point based on location received
     print ("ball", ballX, ballY)
+
     radius = 3.25  # Radius of ball based on 65mm diameter
     box = ((ballX - radius), (ballY - radius), (ballX + radius), (ballY + radius)) # Foradiusmulates the size of the box to crop in pixels (left, upper, right, bottom)
     draw.ellipse(box, fill=128)    # Daw circle around point received
