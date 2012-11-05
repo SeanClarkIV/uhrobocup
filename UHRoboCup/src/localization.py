fb9f58325e9bc79ff65c7eacc26cbca9aeab717a
andressoto93 <andressoto93@c7e3a293-b54f-770b-ae63-5bdf134c42df>
2012-11-05 20:28:56 +0000

    Added part of kalman fillter, doesnt have code in it yet. Added get velocity to localization

--- UHRoboCup/src/localization.py
+++ UHRoboCup/src/localization.py
@@ -74,3 +74,83 @@ def getlocation():
         print "Found red ball position: ", foundredballposition2
         redballtracker.stopTracker()
         return (foundredballposition[0], foundredballposition[1])
+def getvelocity():
+    # Set stiffnes ON
+    config.StiffnessOn()
+
+    #Make sure top camera is active
+    camera.topCamera()
+
+    # Start looking for red ball to track.
+    redballtracker.stopTracker()
+    redballtracker.startTracker()
+    redballtracker.setWholeBodyOn(True)
+    print "Tracking red ball"
+
+    #Store initial red ball positon to variable
+    initialredballposition = redballtracker.getPosition()
+    print "Initial Position: ", initialredballposition
+    cameras = 0 #Top camera
+    count = 0
+    counter = 0 #Used for Redballposition
+    times = 0
+    headpitchangle = 1.433
+    redballposition=initialredballposition
+    redballposition2=redballposition
+    time = 0
+    while redballposition==redballposition2: # runs while loop as long as redballtracker is active
+        while redballtracker.getPosition() == initialredballposition:  # Ball lost.
+            if redballtracker.isNewData() == False and count == 0: # Ball still lost.true if a new Red Ball was detected since the last getPosition().
+                print "Looking for red ball"
+                camera.topCamera()
+                motionProxy.setAngles(['HeadYaw', 'HeadPitch'], [-0.5, headpitchangle], 0.07)
+                time.sleep(3)
+                count = 1
+                cameras = 0 # top camera
+                times += 1
+            elif redballtracker.isNewData() == False and count == 1:
+                camera.bottomCamera()
+                motionProxy.setAngles(['HeadYaw', 'HeadPitch'], [0.5, headpitchangle], 0.07)
+                time.sleep(3)
+                count = 0
+                cameras = 1 # bottom camera
+                times += 1
+            # the next if condition is to look up slightly if he can't find the ball and then when it looks up 3 times
+            # it decides turns left and starts all over again.
+            if times > 1:
+                number = float(times)
+                if number % 2 == 1:
+                    if headpitchangle < 0:
+                        headpitchangle = 1.433
+                        walk.ultimatewalkto(0, 0, 2)
+                    elif headpitchangle > .5:
+                        headpitchangle = .2
+                    else:
+                        headpitchangle = headpitchangle - 0.2
+                    print headpitchangle
+                else:
+                    pass
+            if time == 0:
+                redballposition = redballtracker.getposition()
+                redballposition2 = redballposition
+                time = time.time()
+            elif redballtracker.getposition == redballposition:
+                redballposition2 = redballtracker.getposition()
+                time2 = time.time()
+
+        # Ball found.
+        # Store found red ball positon to variable.
+        #foundredballposition and foundredballposition 2 are to make sure there are no
+        #random jumps so the robot doesn't randomly pick a different thing to go to.
+        xi = redballposition[0]
+        yi = redballposition[1]
+        xf = redballposition2[0]
+        yf = redballposition2[1]
+        deltax = xf-xi
+        deltay = yf-yi
+        deltat = time2-time
+        vx = deltax/deltat
+        vy = deltay/deltat
+        print "velocity x is: ", vx, "velocity y is: ", vy
+        redballtracker.stopTracker()
+        return (vx, vy)
