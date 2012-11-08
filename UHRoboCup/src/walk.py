3691b4494e1a23ed7a38ec7dbdc562ed6c401e68
andressoto93 <andressoto93@c7e3a293-b54f-770b-ae63-5bdf134c42df>
2012-11-08 21:18:23 +0000

    Kalman filter!!!!! it works wellish, still working on it. has goalie that cuts the angle off of the ball. Works well, option 29

--- UHRoboCup/src/walk.py
+++ UHRoboCup/src/walk.py
@@ -1,7 +1,9 @@
+import walk
 import config
-
+import camera
+import time
 motionProxy = config.loadProxy("ALMotion")
-
+redballtracker = config.loadProxy("ALRedBallTracker")
 def ewalk():
 
     #This walk walks for a walkTime amount of time and then stops.
@@ -39,3 +41,69 @@ def ultimatewalkto(x, y, z):
           ["StepHeight", 0.015],       # step height of x cm
           ["TorsoWx", 0],           # default value
           ["TorsoWy", -0.1] ])         # torso bend 0.1 rad in front
+def aligngoalie():
+    config.StiffnessOn()
+    config.PoseInit()
+    #Make sure top camera is active
+    camera.topCamera()
+
+    # Start looking for red ball to track.
+    redballtracker.stopTracker()
+    redballtracker.startTracker()
+    redballtracker.setWholeBodyOn(True)
+    print "Tracking red ball"
+
+    #Store initial red ball positon to variable
+    initialredballposition = redballtracker.getPosition()
+    print "Initial Position: ", initialredballposition
+    cameras = 0 #Top camera
+    count = 0
+    counter = 0 #Used for Redballposition
+    times = 0
+    headpitchangle = 1.433
+    while redballtracker.isActive(): # runs while loop as long as redballtracker is active
+        while redballtracker.getPosition() == initialredballposition:  # Ball lost.
+            if redballtracker.isNewData() == False and count == 0: # Ball still lost.true if a new Red Ball was detected since the last getPosition().
+                print "Looking for red ball"
+                camera.topCamera()
+                motionProxy.setAngles(['HeadYaw', 'HeadPitch'], [-0.5, headpitchangle], 0.07)
+                time.sleep(3)
+                count = 1
+                cameras = 0 # top camera
+                times += 1
+            elif redballtracker.isNewData() == False and count == 1:
+                camera.bottomCamera()
+                motionProxy.setAngles(['HeadYaw', 'HeadPitch'], [0.5, headpitchangle], 0.07)
+                time.sleep(3)
+                count = 0
+                cameras = 1 # bottom camera
+                times += 1
+            # the next if condition is to look up slightly if he can't find the ball and then when it looks up 3 times
+            # it decides turns left and starts all over again.
+            if times > 1:
+                number = float(times)
+                if number % 2 == 1:
+                    if headpitchangle < 0:
+                        headpitchangle = 1.433
+                        walk.ultimatewalkto(0, 0, 2)
+                    elif headpitchangle > .5:
+                        headpitchangle = .2
+                    else:
+                        headpitchangle = headpitchangle - 0.2
+                    print headpitchangle
+                else:
+                    pass
+        foundredballposition = redballtracker.getPosition()
+        print "Found red ball position: ", foundredballposition
+        redballtracker.stopTracker()
+    walk.ultimatewalkto(0, foundredballposition[1], 0)
+def walkclockwise():
+    #This walk walks for a walkTime amount of time and then stops.
+    useSensors = True
+    motionProxy.setMotionConfig([["ENABLE_FOOT_CONTACT_PROTECTION", True]])
+    angles = motionProxy.getAngles("Head", useSensors)
+    X = 0.0
+    Y = -1.1 * .76
+    Theta = -.15
+    Frequency = 1
+    motionProxy.setWalkTargetVelocity(X, Y, Theta, Frequency)
