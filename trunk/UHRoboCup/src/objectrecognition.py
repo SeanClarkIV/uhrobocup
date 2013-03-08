7da0b917a452522ee2f667f89c9cad9964b236b2
alexandrojv9@gmail.com <alexandrojv9@gmail.com@c7e3a293-b54f-770b-ae63-5bdf134c42df>
2013-03-08 21:01:50 +0000

    Instead of extracting red colors onto another image, a matrix is created with red values represented as 1 and non red values as 0

--- trunk/UHRoboCup/src/objectrecognition.py
+++ trunk/UHRoboCup/src/objectrecognition.py
@@ -1,7 +1,9 @@
 import time
-import config
-from PIL import Image, ImageDraw
 
+from PIL import Image, ImageDraw
+import numpy
+
+import config
 
 def redballdetection():
     origImage = getImage()                      # Get image from NAO Robot
@@ -22,7 +24,7 @@ def getImage():
   """
 
   camProxy = config.loadProxy("ALVideoDevice")
-  resolution = 3    # 1280x960 (4VGA)
+  resolution = 1    # 320x240
   colorSpace = 11   # RGB
 
   videoClient = camProxy.subscribe("python_client", resolution, colorSpace, 5)
@@ -58,23 +60,34 @@ def getImage():
   return origImage
 
 def colorpick(origIm):
+    outfile = open('file.txt', 'w') # Open original image
+
     origWidth = origIm.size[0]      # Get original image's width.
     origHeight = origIm.size[1]     # Get original image's height.
+    print "Dimensions - w:", imgWidth, "h:",
 
-    analysedIm = Image.new("RGB", (origWidth,origHeight), "white")  # Create blank image with same dimendions as original image
+#    analysedIm = Image.new("RGB", (origWidth,origHeight), "white")  # Create blank image with same dimendions as original image
 
-    draw = ImageDraw.Draw(analysedIm)   # Set wich image to draw pixels to.
+#    draw = ImageDraw.Draw(analysedIm)   # Set wich image to draw pixels to.
 
+    matrix = numpy.zeros(shape=(origHeight,origWidth))
     for height in range(0, (origHeight), 1):    # Iterate up to down on image through each pixel.
+        outfile.write("\n")
         for width in range (0, (origWidth), 1): # Iterate left to righ on image through each pixel.
             r, g, b = origIm.getpixel((width,height)) # Save each pixel color to variables r,g,b.
 
             if (r >= 85) and (g <= ((r/4)*3)):  # Find all pixels that have a red hue.
-                draw.point((width,height), fill=(r,g,b))    # Export only red hue pixels to blank image.
+                matrix[height][width] = 1
+                matrix = str(matrix)
+                outfile.write("1")
+            else:
+                outfile.write("0")
+
+#                draw.point((width,height), fill=(r,g,b))    # Export only red hue pixels to blank image.
 #                del draw    # Stop draw process.
 
-    # analysedIm.save("analysed.jpg", "JPEG")
-    # analysedIm.show()
-    #print "done"
+#     analysedIm.save("analysed.jpg", "JPEG")
+#     analysedIm.show()
+#     print "done"
 
-    return analysedIm
+    return matrix
